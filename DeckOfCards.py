2b4b92734bc80617e3882927e1f5784cbe534bc9
ROCKY TRANG <TRANGR@tdm.local>
2023-08-11 16:21:03 +0800

    snap() updated

--- DeckOfCards.py
+++ DeckOfCards.py
@@ -13,8 +13,7 @@ class DeckOfCards:
         """Using the Card class, create list of 52 cards, 13 (2-A) from each suit (Hearts, Diamonds, Spades, Clubs)
         
         Args:
-        valueList (list): A list of 13 strings, one for each card value (2-A).
-        suitList (list): A list of 4 strings, one for each card suit (Hearts, Diamonds, Spades, Clubs).
+        None
 
         Returns:
         cards (list): A list of 52 Card objects.
@@ -69,35 +68,56 @@ class DeckOfCards:
         
         #distribute equal number of cards to each player
         for player_hand in hands:
-            for _ in range(cards_per_player):
+            for i in range(cards_per_player):
                 random_index = random.randrange(len(self.cards))
                 card = self.cards.pop(random_index)
                 player_hand.append(card)
                 
         player_number = 1
         for player_hand in hands:
-            print(f"Player {player_number}:")
-            for card in player_hand:
-                print(card, end = "\n")
+            print(f"Player {player_number}:", player_hand)
             player_number += 1
-            print()
-
+        print()        
+        return(hands)
 
 class Player(DeckOfCards):
-    def show_first_card(self):
-        for hand in self.hands:
-            print(hand[0])
+    def __init__(self):
+        DeckOfCards.__init__(self)
+        
+    def snap(self, number_of_players):
+        central_pile = []
+        play = self.deal(number_of_players)
+        player = 1
+        for i in play:
+            print(f"Player {player} turns over a", i[0])
+            #remove from players hand and into central pile
+            central_pile.append(i[0])
+            i.pop(0)
+            #check to see if the central pile's top two cards are identical
+            if len(central_pile) > 1:
+                if central_pile[-1].value == central_pile[-2].value:
+                    print(f"\nPLAYER {player} YELLS SNAP\n")
+                    #put all the central pile into player's hand
+                    for cards in range(len(central_pile)):
+                        i.append(central_pile.pop(-1))
+            if len(i) == 0:
+                print("Player {player} has run out of cards. game over")
+                break
+            player += 1
+            
+        print("\nCentral pile:", central_pile)
+        print()
+        player_count = 1
+        for i in play:
+            print(f"Player {player_count}:", i)
+            player_count += 1
 
-         
 def main():
     # deck.shuffle()
-    # deck.deal(4)
     # deck.display_deck()
-    deck = DeckOfCards()
-    deck.deal(4)
-    player = Player(deck)
-    player.show_first_card()
-    
+    # deck = DeckOfCards()
+    play = Player()
+    play.snap(4)
 
 if __name__ == "__main__":
     main()
